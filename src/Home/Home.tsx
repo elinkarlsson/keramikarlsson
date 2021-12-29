692910e10c30575a9c9aa18253959e09bdd7a355
Elin Karlsson <elin.karlsson@sigma.se>
2021-12-29 20:43:17 +0100

    Fixat länkar.

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -4,6 +4,7 @@ import imgVas from '../assets/home/vas.jpg'
 import imgKoppar from '../assets/home/koppar.jpg'
 import imgMini from '../assets/mini.jpg'
 import imgVasSt from '../assets/vas-2-st.jpg'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
@@ -11,46 +12,46 @@ export default function Home() {
       <div className='home-gallery'>
         <div className='home-gallery__row'>
           <div className='home-gallery__col-3'>
-            <a href="/shop/skulpturer" className="home-gallery__link">
+            <Link to="/shop/skulpturer" className="home-gallery__link">
               <img width="600" src={imgVas} className="home-gallery__img" />
               <div className="home-gallery__middle">
                 <div className="home-gallery__text">Vaser</div>
               </div>
-            </a>
+            </Link>
           </div>
           <div className='home-gallery__col-3'>
-            <a href="/shop/skulpturer" className="home-gallery__link">
+            <Link to="/shop/skulpturer" className="home-gallery__link">
               <img width="600" src={imgKoppar} className="home-gallery__img" />
               <div className="home-gallery__middle">
                 <div className="home-gallery__text">Koppar</div>
               </div>
-            </a>
+            </Link>
           </div>
           <div className='home-gallery__col-3'>
-            <a href="/shop/skulpturer" className="home-gallery__link">
+            <Link to="/shop/skulpturer" className="home-gallery__link">
               <img width="600" src={imgVas} className="home-gallery__img" />
               <div className="home-gallery__middle">
                 <div className="home-gallery__text">Fat</div>
               </div>
-            </a>
+            </Link>
           </div>
         </div>
         <div className='home-gallery__row'>
           <div className='home-gallery__col-2'>
-            <a href="/shop/vaser" className="home-gallery__link">
+            <Link to="/shop/vaser" className="home-gallery__link">
               <img width="600" src={imgMini} className="home-gallery__img" />
               <div className="home-gallery__middle">
                 <div className="home-gallery__text">Skålar</div>
               </div>
-            </a>
+            </Link>
           </div>
           <div className='home-gallery__col-2'>
-            <a href="/shop/vaser" className="home-gallery__link">
+            <Link to="/shop/vaser" className="home-gallery__link">
               <img width="600" src={imgVasSt} className="home-gallery__img" />
               <div className="home-gallery__middle">
                 <div className="home-gallery__text">Vaser</div>
               </div>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/Thumbnail/Thumbnail.tsx
+++ src/Thumbnail/Thumbnail.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
 import './Thumbnail.css'
 
 export default function Thumbnail({ name, url, imageUrl: image, price }: ThumbnailProps) {
   return (
-    <a href={url} className="thumbnail-link">
+    <Link className="thumbnail-link" to={url}>
       <img width="600" src={image} className="thumbnail-img" />
       <div className="thumbnail-description-name">{name}</div>
       {price && <div className="thumbnail-description-price">{price} kr</div>}
-    </a>
+    </Link>
   )
 }
 
