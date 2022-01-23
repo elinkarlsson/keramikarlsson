04ff2ca76ae115c582c41b27e03bbb61b0bad78d
Elin Karlsson <elin.karlsson@sigma.se>
2022-01-23 12:42:56 +0100

    Lagt till ikoner.

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import './Contact.css'
+import instagramIcon from '../assets/icons/instagram-logo.png'
 
 export default function Contact() {
   document.title = 'Kontakt - KeramiKarlsson';
@@ -7,12 +8,18 @@ export default function Contact() {
       <div className="row">
         <div className="column">
           <h2>Kontakt</h2>
-          <p>Mail: elin.karlsson.92@hotmail.com</p>
-          <p>Telefon: 0703294590</p>
+          <p>
+            <span className="material-icons material-icons-outlined">email</span>
+            elin.karlsson.92@hotmail.com
+          </p>
+          <p>
+            <span className="material-icons material-icons-outlined">phone</span>
+            0703294590
+          </p>
         </div>
         <div className="column">
           <h2>Följ</h2>
-          <a href="https://www.instagram.com/keramikarlsson/" target="_blank">Instagram</a>
+          <a href="https://www.instagram.com/keramikarlsson/" target="_blank"><img className='instagram-icon' src={instagramIcon} />Instagram</a>
         </div>
       </div>
     </div>);
